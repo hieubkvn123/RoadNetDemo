f33beafe6e5a287774de004831dd782bf4d07061
Nong Minh Hieu <hieubkvn123@gmail.com>
2021-02-09 22:08:31 +0800

    Added histogram matching but not too useful

--- api/server.py
+++ api/server.py
@@ -5,6 +5,7 @@ import numpy as np
 
 import json
 from PIL import Image
+from skimage import exposure
 
 from flask import Flask 
 from flask import request 
@@ -30,27 +31,8 @@ def _adjust_lumination(sample_image, image):
 		sample_image = sample_image
 
 	assert isinstance(image, np.ndarray)
-	original = image.copy()
 
-	### Convert both image to LAB ###
-	sample_image_lab = cv2.cvtColor(sample_image, cv2.COLOR_BGR2LAB)
-	image_lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
-
-	l1, a1, b1 = cv2.split(sample_image_lab)
-	l2, a2, b2 = cv2.split(image_lab)
-
-	sample_image_mean = l1.mean()
-	sample_image_std  = l1.std()
-	image_mean        = l2.mean()
-	image_std         = l2.std()
-
-	l2_scaled = (l2 - image_mean) / image_std 
-	l2_transformed = l2_scaled * sample_image_std + sample_image_mean
-	l2_transformed = l2_transformed.astype(np.uint8)
-	l2_transformed[l2_transformed > 255] = 0
-
-	new_lab = cv2.merge((l2_transformed, a2, b2))
-	new_image = cv2.cvtColor(new_lab, cv2.COLOR_LAB2BGR)
+	new_image = exposure.match_histograms(image, sample_image, multichannel=True)
 
 	return new_image
 
@@ -78,6 +60,7 @@ def upload_and_process():
 		image = np.array(image)
 		image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 		# image = _adjust_lumination('sample.tif', image)
+		#cv2.imwrite('test.png', image)
 
 		selected_model = request.form['model']
 
